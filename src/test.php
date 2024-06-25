a77ccac1f098a3fcdedde884ff7759fc7fa9adeb
linjinghui01 <ljh@ljhdeMBP.lan>
2024-06-25 20:05:40 +0800

    Update

--- src/test.php
+++ src/test.php
@@ -13,5 +13,5 @@ $fruitList = $fruitApiService->getFruitList();
 print_r($fruitList);
 
 // Buying Fruits
-$buyResponse = $fruitApiService->buyFruit(1, 2); // 假设 id 为 1，数量为 2
+$buyResponse = $fruitApiService->buyFruit(1, 2); //Assuming id is 1 and quantity is 2
 print_r($buyResponse);
