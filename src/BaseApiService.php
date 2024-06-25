ba71b76e1aaab4ef538a4d367ecd94e262c7ecaf
ljh <ljh@ljhdeMacBook-Pro.local>
2024-06-25 17:29:04 +0800

    Update BaseApiService.php

--- src/BaseApiService.php
+++ src/BaseApiService.php
@@ -1,1 +1,18 @@
 <?php
+
+declare(strict_types=1);
+
+abstract class BaseApiService
+{
+    protected function logRequest($request): void
+    {
+        // Implement logging of request to the database
+    }
+
+    protected function logResponse($response): void
+    {
+        // Implement logging of response to the database
+    }
+
+    // Additional common methods for API services can be added here
+}
